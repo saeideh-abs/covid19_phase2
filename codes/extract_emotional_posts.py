e85669b8aebbadc671326caabcc27ec8253f9099
moosavi <mohammadmirmoosavi@gmail.com>
2020-06-24 20:18:21 +0430

    polarity added

--- codes/extract_emotional_posts.py
+++ codes/extract_emotional_posts.py
@@ -9,21 +9,22 @@ class Extraction():
 
     def __init__(self, input_file):
         self.input_file = input_file
+        self.polarity = ['مثبت', 'منفی', 'خنثی']
         self.emotional_tags = ['شادی', 'غم', 'ترس', 'تنفر', 'خشم', 'شگفتی', 'اعتماد', 'پیش‌بینی', 'سایر هیجانات',
                                'استرس']
 
-    def extract_emotional_posts(self):
+    def extract_emotional_posts(self, tags):
         dataframe = pd.read_csv(self.input_file)
-        numpy_emotional_array = dataframe.loc[:, self.emotional_tags].to_numpy()
+        numpy_emotional_array = dataframe.loc[:, tags].to_numpy()
         sum_array = np.sum(numpy_emotional_array, axis=1)
         row_arg_list = np.where(sum_array > 0)[0].tolist()
         return row_arg_list, dataframe
 
-    def write_emotional_posts_to_csv(self, row_args_list, dataframe):
+    def write_emotional_posts_to_csv(self, row_args_list, dataframe, folder_name, file_name):
         target_df = dataframe.loc[row_args_list, :]
-        folder = '{}/data/emotional_posts'.format(path)
+        folder = '{}/data/{}'.format(path, folder_name)
         os.makedirs(name=folder, exist_ok=True)
-        target_df.to_csv('{}/emotional_posts.csv'.format(folder))
+        target_df.to_csv('{}/{}.csv'.format(folder, file_name))
         return target_df
 
 
@@ -32,5 +33,8 @@ path = os.getcwd()
 parent_dir = os.path.abspath(os.path.join(path, os.pardir))
 
 ex_instance = Extraction('{}/data/manual_tag/clean_labeled_data.csv'.format(path))
-row_args_list, dataframe = ex_instance.extract_emotional_posts()
-final_df = ex_instance.write_emotional_posts_to_csv(row_args_list, dataframe)
+row_args_list, dataframe = ex_instance.extract_emotional_posts(ex_instance.emotional_tags)
+final_df_emotions = ex_instance.write_emotional_posts_to_csv(row_args_list, dataframe, 'statistics', 'emotions')
+
+row_args_list, dataframe = ex_instance.extract_emotional_posts(ex_instance.polarity)
+final_df_polarity = ex_instance.write_emotional_posts_to_csv(row_args_list, dataframe, 'statistics', 'polarity')

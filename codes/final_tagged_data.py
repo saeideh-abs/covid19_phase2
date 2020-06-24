410d06c89e5dad61792b2cd76208e4afba7eacda
saeideh abs <saeideh676@gmail.com>
2020-06-24 09:18:04 +0430

    combine the old version  with telegram data

--- codes/final_tagged_data.py
+++ codes/final_tagged_data.py
@@ -5,10 +5,11 @@ if __name__ == '__main__':
     instagram = pd.read_csv('../data/manual_tag/all_fields_instagram.csv')
     twitter = pd.read_csv('../data/manual_tag/all_fields_twitter.csv')
     news = pd.read_csv('../data/manual_tag/all_fields_news.csv')
-
-    all_fields_df = pd.concat([instagram, twitter, news], axis=0, ignore_index=True)
+    telegram = pd.read_csv('../data/manual_tag/all_fields_telegram.csv')
+    all_fields_df = pd.concat([instagram, twitter, news, telegram], axis=0, ignore_index=True)
     print(len(instagram))
     print(len(twitter))
     print(len(news))
+    print(len(telegram))
 
     all_fields_df.to_csv("../data/manual_tag/all_fields.csv", index=False)

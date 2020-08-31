5a134e72bec512f3e474b9c666de4fdfae72913b
saeideh abs <saeideh676@gmail.com>
2020-08-31 18:35:27 +0430

    return topic index of each post as new column

--- codes/classification/topic_modeling.py
+++ codes/classification/topic_modeling.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 import pandas as pd
 import numpy as np
+import time
+import json
 from hazm import *
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
@@ -8,6 +10,12 @@ from sklearn.exceptions import ConvergenceWarning
 import warnings
 warnings.simplefilter("ignore", DeprecationWarning)
 warnings.simplefilter("ignore", ConvergenceWarning)
+
+
+def display_current_time():
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    return current_time
 
 
 def hazm_sentences_tokenize(docs, joined=True, numpy_array=True):
@@ -29,22 +37,36 @@ def hazm_sentences_tokenize(docs, joined=True, numpy_array=True):
 
 
 def NMF_topic_modeling(docs, no_features, no_topics, no_top_words):
-    print("NMF topic modeling")
     # NMF is able to use tf-idf
     tfidf_vecs, tfidf_feature_names = build_tfidf(docs, no_features=no_features)
     # Run NMF
     nmf = NMF(n_components=no_topics, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(tfidf_vecs)
-    display_topics(nmf, tfidf_feature_names, no_top_words)
+    topics_dict = return_topics(nmf, tfidf_feature_names, no_top_words)
+    display_topics(topics_dict)
+
+    # find each post is related to which topic?
+    doc_topic = nmf.transform(tfidf_vecs)
+    # find index of the best topic (topic with highest score) for each document
+    doc_best_topic = np.argmax(doc_topic, axis=1)
+    # print(len(doc_best_topic), doc_best_topic[0:20])
+    return topics_dict, doc_best_topic
 
 
 def LDA_topic_modeling(docs, no_features, no_topics, no_top_words):
-    print("LDA topic modeling")
     # LDA can only use raw term counts for LDA because it is a probabilistic graphical model
     tf_vecs, tf_feature_names = build_tf(docs, no_features=no_features)
     # Run LDA
     lda = LatentDirichletAllocation(n_components=no_topics, max_iter=5, learning_method='online',
-                                    learning_offset=50., random_state=0).fit(tf_vecs)
-    display_topics(lda, tf_feature_names, no_top_words)
+                                    learning_offset=50., random_state=0, n_jobs=-1).fit(tf_vecs)
+    topics_dict = return_topics(lda, tf_feature_names, no_top_words)
+    display_topics(topics_dict)
+
+    # find each post is related to which topic?
+    doc_topic = lda.transform(tf_vecs)
+    # find index of the best topic (topic with highest score) for each document
+    doc_best_topic = np.argmax(doc_topic, axis=1)
+    # print(len(doc_best_topic), doc_best_topic)
+    return topics_dict, doc_best_topic
 
 
 def build_tfidf(docs, no_features):
@@ -61,28 +83,55 @@ def build_tf(docs, no_features):
     return tf, tf_feature_names
 
 
-def display_topics(model, feature_names, no_top_words):
+def return_topics(model, feature_names, no_top_words):
+    keys = []
+    values = []
     for topic_idx, topic in enumerate(model.components_):
-        print("Topic %d:" % (topic_idx))
-        print(" ".join([feature_names[i]
-                        for i in topic.argsort()[:-no_top_words - 1:-1]]))
+        topic_name = "topic" + str(topic_idx)
+        topic_words = " ".join([feature_names[i]
+                        for i in topic.argsort()[:-no_top_words - 1:-1]])
+        keys.append(topic_name)
+        values.append(topic_words)
+    return dict(zip(keys, values))
+
+
+def display_topics(topics_dict):
+    for topic_name, topic_words in topics_dict.items():
+        print(topic_name, ":")
+        print(topic_words)
 
 
 if __name__ == '__main__':
     # _______________ loading the data and preprocessing _______________
     dataset = pd.read_csv('../../data/politics.csv', na_values='')
+
+    print("search and remove null values", display_current_time())
     nan_indices = np.where(dataset['textField_nlp_normal'].isnull())[0]  # get index of null values
-    documents = dataset.drop(nan_indices, axis=0)['textField_nlp_normal']  # remove null values
-    print("preprocessing using hazm")
+    dataset = dataset.drop(nan_indices, axis=0)  # remove null values
+    documents = dataset['textField_nlp_normal']
+
+    print("preprocessing using hazm", display_current_time())
     documents = hazm_sentences_tokenize(documents, numpy_array=False)
 
+    # _______________ topic modeling part _______________
     number_of_features = 1000
     number_of_topics = 10
     number_of_top_words = 10
 
-    NMF_topic_modeling(documents, no_features=number_of_features,
+    print("NMF topic modeling", display_current_time())
+    nmf_topics_dictionary, nmf_document_best_topic = NMF_topic_modeling(documents, no_features=number_of_features,
                        no_topics=number_of_topics,
                        no_top_words=number_of_top_words)
-    LDA_topic_modeling(documents, no_features=number_of_features,
+
+    print("LDA topic modeling", display_current_time())
+    lda_topics_dictionary, lda_document_best_topic = LDA_topic_modeling(documents, no_features=number_of_features,
                        no_topics=number_of_topics,
                        no_top_words=number_of_top_words)
+    print(display_current_time())
+
+    # _______________ write results _______________
+    dataset["topic_index"] = nmf_document_best_topic
+    dataset.to_csv('../../data/politics_with_topics.csv', index=False)
+
+    with open('../../data/nmf_topics_dictionary.json', 'w', encoding='utf-8') as fp:
+        json.dump(nmf_topics_dictionary, fp, ensure_ascii=False)
